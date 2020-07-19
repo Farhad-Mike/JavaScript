538d4c61d0cabd2c42f1d35c2a2bf9f68dd983a0
Farhad-Mike <54776279+Farhad-Mike@users.noreply.github.com>
2020-07-19 04:50:06 +0400

    u

--- events.js
+++ events.js
@@ -1,14 +1,20 @@
 click           // происходит, когда кликнули на элемент левой кнопкой мыши (на устройствах с сенсорными экранами оно происходит при касании).
+dbclick         // Вызывается двойным кликом на элементе.
 contextmenu     // происходит, когда кликнули на элемент правой кнопкой мыши.
 mouseover       // когда мышь наводится на
 mouseout        // покидает элемент.
 mousedown       // когда нажали
 mouseup         // отжали кнопку мыши на элементе.
 mousemove       // при движении мыши.
+mouseenter      // Переходы внутри элемента, на его потомки и с них, не считаются. События mouseenter/mouseleave не всплывают.
+mouserleave     // Переходы внутри элемента, на его потомки и с них, не считаются. События mouseenter/mouseleave не всплывают.
 submit          // пользователь отправил форму <form>.
 focus           // пользователь фокусируется на элементе, например нажимает на <input>.
 keydown         // когда пользователь нажимает
 keyup           // отпускает клавишу.
+dragstart       // начать Drag’n’Drop
+dragend         // закончить Drag’n’Drop
+scroll          // позволяет реагировать на прокрутку страницы или элемента
 DOMContentLoaded    // когда HTML загружен и обработан, DOM документа полностью построен и доступен.
 transitionend   // когда CSS-анимация завершена
 
@@ -27,33 +33,31 @@ event.touches[0].clientX; // the prop isn't exist for .ontouchend;
 event.touches[0].clientY;
 event.changedTouches[0].clientX;
 event.changedTouches[0].clientY; // the prop exist in all touch events;
-event.target.name;      // get name attr from input elem and matches elements;
 event.type;             // Тип события, в данном случае "click".
 event.currentTarget;    // Элемент, на котором сработал обработчик. Значение – обычно такое же, как и у this, но если обработчик является функцией-стрелкой или при помощи bind привязан другой объект в качестве this, то мы можем получить элемент из event.currentTarget
 event.target;           // the most bottom element on whitch has called event; Самый глубокий элемент, который вызывает событие.  это «целевой» элемент, на котором произошло событие, в процессе всплытия он неизменен.
+event.relatedTarget;    //  это элемент, с которого курсор ушёл (relatedTarget → target) или это элемент, на который курсор перешёл (target → relatedTarget). Свойство relatedTarget может быть null Это нормально и означает, что указатель мыши перешёл не с другого элемента, а из-за пределов окна браузера. Или же, наоборот, ушёл за пределы окна..
 event.stopPropagation(); // останавливает всплытие события, но на самом элементе обработаются все соответствующиеся события.
 event.stopImmediatePropagation(); // останавливает всплытие и так же предотвращает все соответствующие события на самом элементе.
 event.preventDefault(); // Для отмены действия браузера по умолчанию при срабатывании события. event.preventDefault() передаст false для кастомного event в elem.dispatchEvent(event)
 event.defaultPrevented; // установлено в true, если действие по умолчанию было предотвращено, и false, если нет.
 event.eventPhase;       // 1 capture or 2 target or 3 bubble;
-event.relatedTarget;
 event.which;            // 1 == left, 2 == center, 3 == right mouse buttons; and for discover keyboard keydown, keyup, keypress events;
 event.deltaY;           // how much has been scrroled with wheel;
 event.deltaX;
 event.deltaZ;
-event.keyCode;          // work on all browser except on the IE. Works like event.which; keyCode work only for keydown and keyup;
-event.charCode;         // work only for event 'keypress' and only for symbol;
-{ keydown, keypress/*only for symbols and return symbol for lower or uppercase*/, keyup }
-event.shiftKey;
-event.ctrlKey;
-event.metaKey;
-event.altKey;
+event.code;             // физический код клавиши
+event.key;              // позволяет получить символ
+event.repeat;           // Для событий, вызванных автоповтором, у объекта события свойство event.repeat равно true
+event.shiftKey;         // равно true, если во время события была нажата соответствующая клавиша.
+event.ctrlKey;          // равно true, если во время события была нажата соответствующая клавиша.
+event.metaKey;          // равно true, если во время события была нажата соответствующая клавиша.
+event.altKey;           // равно true, если во время события была нажата соответствующая клавиша.
 event.lengthComputable; // false/true неизвестный рамер либо нулевой размер тела. Только при получении ответа с сервера.
 event.loaded;           // Кол-во загруженных байтов.
 event.total;            // Общее кол-во байтов.
 event.reason;           // причина закрытия соединения.
 event.wasClean;         // true/false чистое закрытие или обрыв
-event.code;
 event.origin;           // При кросс-доменных запросах появится. Откуда пришли данные (домен);
 event.reason;
 event.data;
