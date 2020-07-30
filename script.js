b8de468a5fcea2f2a770519f8149365168e0e0b4
Farhad-Mike <54776279+Farhad-Mike@users.noreply.github.com>
2020-07-31 01:05:51 +0400

    u

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 'use strict';
 
 
-function concat(arr) {
-    
-}
+let a = new Uint8Array([1, 2, 3, 4]);
+let b = new Uint8Array([5, 6, 7, 8]);
+
+let c = new Uint8Array(8);
+
+c.set(a, 0);
+c.set(b, 4);
+
+console.log(c)
