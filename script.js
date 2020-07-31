ebc6f7e08cd1b3382c5547c9e54556ae06f37090
Farhad-Mike <farhadmike@users.noreply.github.com>
2020-08-01 01:26:52 +0400

    u

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 'use strict';
 
 
-function concat(arr) {
-    
-}
+let a = new Uint32Array(2);
+let b = new Uint8Array(8);
+
+let c = new Uint32Array(8);
+
+// c.set(a);
+// c.set(b);
+
+console.log(a)
+console.log(a.BYTES_PER_ELEMENT)
+
