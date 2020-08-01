51513025b19cb44d0afe4fc5babf9f12e7b75970
Farhad-Mike <54776279+Farhad-Mike@users.noreply.github.com>
2020-08-01 14:50:45 +0400

    u

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 'use strict';
 
+let app = new Vue({
+    el: '#vue-app',
+    data: {
+        name: 'Farhad',
+        title: 'Maharramov',
+        show: true,
+        items: [
+            {text: 'Hello'},
+            {text: 'My'},
+            {text: 'Name'}
+        ]
+    },
 
-let formData = new FormData(form);
-
-console.log(formData.has('name'))
+    methods: {
+        showConsole(e) {
+            console.log(e.target.textContent)
+        }
+    }
+})
