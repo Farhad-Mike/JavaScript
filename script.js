be8eb98cca3f55eea03b69b01444bc51e1280564
Farhad-Mike <54776279+Farhad-Mike@users.noreply.github.com>
2020-08-04 03:41:47 +0400

    u

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+Vue.component('item', {
+    props: ['param'],
+    template: '<li>{{ param.text }}</li>'
+})
+
+new Vue({
+    el: '#app',
+    data: {
+        messages: [
+            { id: 1, text: 'Hello' },
+            { id: 2, text: 'My' },
+            { id: 3, text: 'Name' }
+        ]
+    }
+})
