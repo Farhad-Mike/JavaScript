c5db48c1920228df33f6403964f017cdaf127f28
Farhad-Mike <54776279+Farhad-Mike@users.noreply.github.com>
2020-08-04 00:35:48 +0400

    u

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
 'use strict';
 
-(async function getRequest() {
-    let responce = fetch('https://next.json-generator.com/api/json/get/V1enUzl-Y', {
-        method: 'GET'
-    }).then(resolve => {
-        console.log(resolve.headers.get('cache-control'))
-        return resolve.json();
-    }).then(resolve => console.log(resolve))
-}())
